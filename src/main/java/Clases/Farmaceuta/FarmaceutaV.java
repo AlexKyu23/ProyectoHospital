3d7f1aa54a2a64ce62e87eca1c2092d463f7a7ef
Steven2305 <stevenjosepalmaportilla@gmail.com>
2025-09-01 09:38:35 -0600

    View Farmaceuta

--- src/main/java/Clases/Farmaceuta/FarmaceutaV.java
+++ src/main/java/Clases/Farmaceuta/FarmaceutaV.java
@@ -1,4 +1,18 @@
 package Clases.Farmaceuta;
 
+import javax.swing.*;
+
 public class FarmaceutaV {
+    private JPanel panel1;
+    private JPanel Farmaceuta;
+    private JTextField ID;
+    private JTextField Nombre;
+    private JButton guardarButton;
+    private JButton LImpiarButton;
+    private JButton borrarButton;
+    private JTextField NombreB;
+    private JButton buscarButton;
+    private JButton reporteButton;
+    private JPanel Listado;
+    private JTable table1;
 }
---
+++ src/main/java/Clases/Prescribir/Controller.java
@@ -0,0 +1,4 @@
+package Clases.Prescribir;
+
+public class Controller {
+}
