608c854433d0d6514201dc2bf2e906e4de1e42d5
Steve2305 <stevenjosepalmaportilla@gmail.com>
2025-09-03 22:27:46 -0600

    View Prescripcion

--- src/main/java/Clases/Prescribir/PrescribirView.java
+++ src/main/java/Clases/Prescribir/PrescribirView.java
@@ -1,4 +1,19 @@
 package Clases.Prescribir;
 
+import javax.swing.*;
+
 public class PrescribirView {
+    private JButton buscarPacienteButton;
+    private JButton agregarMedicamentoButton;
+    private JTextField textField1;
+    private JLabel NombrePaciente;
+    private JTable MedicamentosPreenscritos;
+    private JButton guardarButton;
+    private JButton limpiarButton;
+    private JButton descartarMedicamentoButton;
+    private JButton detallesButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
