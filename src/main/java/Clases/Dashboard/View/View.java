e4409ca298de470a18f04d71471f38e3c2365819
alissazmr <alissazmr@gmail.com>
2025-09-07 18:53:53 -0600

    Add files via upload

---
+++ src/main/java/Clases/Dashboard/View/View.java
@@ -0,0 +1,18 @@
+package Clases.Dashboard.View;
+
+import javax.swing.*;
+
+public class View {
+    private JPanel Datos;
+    private JPanel Medicamentos;
+    private JPanel Recetas;
+    private JPanel Desde_hasta;
+    private JComboBox desde_año;
+    private JComboBox hasta_año;
+    private JComboBox desde_diaMes;
+    private JComboBox hasta_diaMes;
+    private JComboBox medicamentos;
+    private JButton checkButton;
+    private JButton doubleCheckButton;
+    private JPanel Lista;
+}
