ef65f35026cd2401401ed770639b4168db6c8cb8
AlexKyu23 <jalexartola@gmail.com>
2025-09-11 20:40:24 -0600

    ya matenme

--- src/main/java/Clases/Farmaceuta/logic/FarmaceutaService.java
+++ src/main/java/Clases/Farmaceuta/logic/FarmaceutaService.java
@@ -1,47 +1,49 @@
 package Clases.Farmaceuta.logic;
 
-import Clases.Farmaceuta.data.FarmaceutaData;
+
+import Clases.Farmaceuta.data.ListaFarmaceutas;
 
 import java.util.List;
 
 public class FarmaceutaService {
     private static FarmaceutaService instance;
-    private FarmaceutaData data;
-
-    private FarmaceutaService() {
-        data = new FarmaceutaData();
-    }
+    private ListaFarmaceutas lista;
 
     public static FarmaceutaService instance() {
         if (instance == null) instance = new FarmaceutaService();
         return instance;
     }
 
-    public void create(Farmaceuta f) throws Exception {
-        if (readById(f.getId()) != null)
-            throw new Exception("Farmaceuta ya existe");
-        data.getFarmaceutas().add(f);
-    }
-
-    public Farmaceuta readById(String id) {
-        return data.getFarmaceutas().stream()
-                .filter(f -> f.getId().equalsIgnoreCase(id))
-                .findFirst()
-                .orElse(null);
-    }
-
-    public Farmaceuta readByNombre(String nombre) {
-        return data.getFarmaceutas().stream()
-                .filter(f -> f.getNombre().equalsIgnoreCase(nombre))
-                .findFirst()
-                .orElse(null);
-    }
-
-    public void delete(String id) {
-        data.getFarmaceutas().removeIf(f -> f.getId().equalsIgnoreCase(id));
+    private FarmaceutaService() {
+        lista = new ListaFarmaceutas();
+        // Podés cargar desde XML aquí si querés persistencia
     }
 
     public List<Farmaceuta> findAll() {
-        return data.getFarmaceutas();
+        return lista.consulta();
+    }
+
+    public void create(Farmaceuta f) throws Exception {
+        if (lista.busquedaPorId(f.getId()) != null) {
+            throw new Exception("Ya existe un farmaceuta con ese ID");
+        }
+        f.setClave(f.getId()); // clave = id al crear
+        lista.inclusion(f);
+    }
+
+    public void delete(String id) {
+        lista.borrado(id);
+    }
+
+    public Farmaceuta readById(String id) {
+        return lista.busquedaPorId(id);
+    }
+
+    public Farmaceuta readByNombre(String nombre) {
+        return lista.busquedaPorNombre(nombre);
+    }
+
+    public void update(Farmaceuta f) {
+        lista.modificacion(f);
     }
 }
