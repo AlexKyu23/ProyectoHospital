8c7e54a1d954827eec08866325dbc575709e9559
alissazmr <alissazmr@gmail.com>
2025-10-18 21:03:16 -0600

    Receta DAO

---
+++ src/main/java/Datos/RecetaDAO.java
@@ -0,0 +1,112 @@
+package Datos;
+
+import Clases.Receta.logic.Receta;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecetaDAO {
+
+    Database db;
+
+    public RecetaDAO() {
+        db = Database.instance();
+    }
+
+    public void guardar(Receta r) throws Exception {
+        String sql = "INSERT INTO Receta (id, medicoId, pacienteId, fechaConfeccion, fechaRetiro) VALUES (?,?,?,?,?)";
+
+        String confeccionString = r.getFechaConfeccion().toString();
+        String retiroString = r.getFechaRetiro().toString();
+
+        PreparedStatement ps = db.prepareStatement(sql);
+        ps.setString(1, r.getId());
+        ps.setString(2, r.getMedicoId());
+        ps.setString(3, r.getPacienteId());
+        ps.setString(4, confeccionString);
+        ps.setString(5, retiroString);
+        int count = db.executeUpdate(ps);
+
+        if (count == 0) {
+            throw new Exception("Receta ya existe");
+        }
+    }
+
+    public void actualizar(Receta r) throws Exception {
+        String sql = "UPDATE Receta SET medicoId=?, pacienteId=?, fechaConfeccion=?, fechaRetiro=? WHERE id=?";
+
+        String confeccionString = r.getFechaConfeccion().toString();
+        String retiroString = r.getFechaRetiro().toString();
+
+        PreparedStatement ps = db.prepareStatement(sql);
+        ps.setString(1, r.getMedicoId());
+        ps.setString(2, r.getPacienteId());
+        ps.setString(3, confeccionString);
+        ps.setString(4, retiroString);
+        int count = db.executeUpdate(ps);
+
+        if (count == 0) {
+            throw new Exception("Receta ya existe");
+        }
+        if (count == 0) {
+            throw new Exception("Receta no existe");
+        }
+    }
+
+    public Receta buscarPorId(String id) throws Exception {
+        try {
+            String sql = "SELECT * FROM Receta WHERE id=?";
+
+            PreparedStatement ps = db.prepareStatement(sql);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new Receta(
+                        rs.getString("id"),
+                        rs.getString("medicoId"),
+                        rs.getString("pacienteId"),
+                        rs.getDate("fechaConfeccion").toLocalDate(),
+                        rs.getDate("fechaRetiro").toLocalDate());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void borrar(Receta o) throws Exception {
+        String sql = "DELETE FROM Receta WHERE id=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, o.getId());
+        int count = db.executeUpdate(stm);
+        if (count == 0) {
+            throw new Exception("Receta no existe");
+        }
+    }
+
+    public List<Receta> listar() throws Exception {
+        List<Receta> lista = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM Medicamento";
+            PreparedStatement stm = db.prepareStatement(sql);
+            ResultSet rs =  db.executeQuery(stm);
+            while (rs.next()) {
+                lista.add(new Receta(
+                        rs.getString("id"),
+                        rs.getString("medicoId"),
+                        rs.getString("pacienteId"),
+                        rs.getDate("fechaConfeccion").toLocalDate(),
+                        rs.getDate("fechaRetiro").toLocalDate()));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+}
